8237193baf41dddb64f3aae829bb99f5398ee1e5
Alex Gorbenko <gorbenkoav@gmail.com>
2018-01-25 15:52:25 +0200

    Add test data and test clients Implement corner cases

--- seam-carving/src/SeamCarver.java
+++ seam-carving/src/SeamCarver.java
@@ -1,5 +1,7 @@
 import edu.princeton.cs.algs4.Picture;
 
+import java.util.function.IntSupplier;
+
 public class SeamCarver {
 
     private Picture picture;
@@ -10,6 +12,7 @@ public class SeamCarver {
      * @param picture source image
      */
     public SeamCarver(Picture picture) {
+        validate(picture);
         this.picture = new Picture(picture);
     }
 
@@ -19,7 +22,7 @@ public class SeamCarver {
      * @return current picture
      */
     public Picture picture() {
-        return null;
+        return picture;
     }
 
     /**
@@ -28,29 +31,36 @@ public class SeamCarver {
      * @return width of current picture
      */
     public int width() {
-        return 0;
+        return picture.width();
     }
 
     /**
      * Height of current picture
+     *
      * @return height of current picture
      */
     public int height() {
-        return 0;
+        return picture.height();
     }
 
     /**
      * Energy of pixel at column x and row y
+     *
      * @param x column index
      * @param y row index
      * @return energy of pixel
      */
     public double energy(int x, int y) {
+        validateRange(x, width());
+        validateRange(y, height());
+
+
         return 0.0;
     }
 
     /**
      * Sequence of indices for horizontal seam
+     *
      * @return sequence of indices for horizontal seam
      */
     public int[] findHorizontalSeam() {
@@ -72,14 +82,48 @@ public class SeamCarver {
      * @param seam to be removed
      */
     public void removeHorizontalSeam(int[] seam) {
-
+        validateSeam(seam, this::width);
     }
 
     /**
      * Remove vertical seam from current picture
+     *
      * @param seam to be removed
      */
     public void removeVerticalSeam(int[] seam) {
+        validateSeam(seam, this::height);
+    }
 
+    private void validate(Object arg) {
+        if (arg == null) {
+            throw new IllegalArgumentException("Argument must not be null");
+        }
+    }
+
+    private void validateRange(int index, int max) {
+        if (index < 0 || index >= max) {
+            throw new IllegalArgumentException("Index " + index + "should be between 0 and " + (max - 1));
+        }
+    }
+
+    private void validateSeam(int[] seam, IntSupplier dimensionSize) {
+        validate(seam);
+        int dim = dimensionSize.getAsInt();
+
+        if (seam.length != dim) {
+            throw new IllegalArgumentException("Size of picture is too small");
+        }
+
+        for (int i = 0; i < seam.length; i++) {
+            validateRange(seam[i], dim);
+            if (i != 0 && seam[i] - seam[i-1] > 1) {
+                throw new IllegalArgumentException("Diff between 2 neighbour entries more than one");
+            }
+
+        }
+
+        if (dimensionSize.getAsInt() <= 1) {
+            throw new IllegalArgumentException("Size of picture is too small");
+        }
     }
 }
